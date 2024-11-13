dd22a95e8f45c9966437fda2750a1246d32b2dee
60minuteman <6teeminuteman@gmail.com>
2024-11-13 20:43:51 +0100

    fix: add user routes to server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth.routes');
+const userRoutes = require('./routes/user.routes');
 
 dotenv.config();
 
@@ -59,6 +60,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 // Error handling
 app.use((err, req, res, next) => {
